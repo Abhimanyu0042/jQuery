784ab6da8fb920ff83a648d852c8614c3e341194
Abhimanyu0042 <abhimanyu0042.be21@chitkara.edu.in>
2022-04-13 11:46:28 +0530

    jQuery Practice

---
+++ jQueryDay3.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+    //it takes time (in seconds) as an argument 
+    //$('#learning').hide(4000)
+
+    //$('#learning').hide(3000, function(){
+    //      console.log("content hiding completed....!")
+    //});
+
+    //$('#learning').show(5000, function(){
+    //    alert("content comes back...!")
+    //});
+
+    //ON BUTTON CLICK EVENT FIRE
+    //$('#btn1').click(function(){
+    //    $('#learning').toggle(1000);
+    //});
+
+    //EXERCISE ON FADING IN/OUT 
+    //fadeIn()
+    //fadeOut()
+    //fadeToggle()
+    //fadeTo()
+
+    //FADEOUT ILLUSTRATION
+    //$('#btn1').click(function(){
+    //    $('#learning').fadeOut(1000);
+    //});
+
+    //FADEIN AND FADEOUT ARE USED IN PAIRS ONLY
+
+    //FADEIN ILLUSTRATION
+    //$('#btn1').click(function(){
+    //    $('#learning').fadeIn(1000);
+    //});
+    
+    //SIMILARLY FADETOGGLE AND FADETO ARE ALSO USED IN PAIRS
+
+    //FADETOGGLE ILLUSTRATION
+    //$('#btn1').click(function(){
+    //$('#learning').fadeToggle(1000);
+    //});
+
+    //FADETO ILLUSTRATION
+    //$('#btn1').click(function(){
+    //$('#learning').fadeTo(1000);
+    //});
+
+    //SLIDING TEXT METHODS note: It takes two arguments speed and call
+    //$('#btn1').click(function(){
+    //    $('#learning').slideUp(3000)
+    //    console.log("Done...!")
+    //});
+
+    //$('#btn1').click(function(){
+    //    $('#learning').slideDown(3000)
+    //    console.log("Done...!")
+    //});
+
+    //ANIMATE METHODS 
+
+    //$('#learning').animate({ 
+    //    opacity:0.3,
+    //    height:'150px',
+    //    width:'350px',
+    //}, 4000)
+
+    $('#learning').animate({ 
+        opacity:0.3,
+        height:'150px',
+        width:'350px',
+    }, "slow")
+
+
+});
